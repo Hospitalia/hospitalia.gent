7d1017d3654aa1bebb45aa1394da51e7fecadc0c
manatran <manatran@student.arteveldehs.be>
2020-03-08 02:25:31 +0100

    Edited page title

--- src/pages/praesidium.js
+++ src/pages/praesidium.js
@@ -24,7 +24,7 @@ const Praesidium = () => {
     return (
         <Layout>
             <div className="container page">
-                <SEO title="Page two" />
+                <SEO title="Praesidium" />
                 <h2>Praesidium</h2>
                 <p>
                     Het praesidium is verantwoordelijk voor het organiseren van
